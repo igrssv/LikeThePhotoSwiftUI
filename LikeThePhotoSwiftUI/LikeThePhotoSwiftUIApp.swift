f7994e262ba9bdce855291f8cd3cf5e1dad1475d
Igor Sysoev <igro.sia@yandex.ru>
2023-03-12 09:19:08 +0300

    Initial Commit

---
+++ LikeThePhotoSwiftUI/LikeThePhotoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeThePhotoSwiftUIApp.swift
+//  LikeThePhotoSwiftUI
+//
+//  Created by Игорь Сысоев on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LikeThePhotoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
